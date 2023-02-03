b75a5bb41d30e8491d89d3fb140b0e748925970c
Md. Zarif Ul Alam <zarif98sjs@gmail.com>
2023-02-03 14:40:08 +0600

    `maxpool` forward prop added, tested with 2 different implementation

--- train_fast_v1.py
+++ train_fast_v1.py
@@ -111,3 +111,141 @@ class Conv2D:
 
         return dX, dW, db
 
+class MaxPool2D:
+    def __init__(self, kernel_size, stride=1, padding=0):
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.padding = padding
+
+        self.cache = None
+        self.method = None
+
+    def get_matrix_indices(self, x_shape):
+        _ , C, H, W = x_shape
+        H_out = (H + 2*self.padding - self.kernel_size) // self.stride + 1
+        W_out = (W + 2*self.padding - self.kernel_size) // self.stride + 1
+
+        """
+        creating the indexes needed to convert the matrix into consecutive columns
+        """
+
+        ## index i
+        i0 = np.repeat(np.arange(self.kernel_size), self.kernel_size)
+        i0 = np.tile(i0, C)
+        all_levels = self.stride * np.repeat(np.arange(H_out), W_out)
+        i = i0.reshape(-1, 1) + all_levels.reshape(1, -1)
+
+        ## index j
+        j0 = np.tile(np.arange(self.kernel_size), self.kernel_size * C)
+        all_slides = self.stride * np.tile(np.arange(W_out), H_out)
+        j = j0.reshape(-1, 1) + all_slides.reshape(1, -1)
+
+        ## index d
+        d = np.repeat(np.arange(C), self.kernel_size * self.kernel_size).reshape(-1, 1)
+
+        return i, j, d
+
+    def im2col(self, x):
+        img = np.pad(x, [(0, 0), (0, 0), (self.padding, self.padding), (self.padding, self.padding)], 'constant')
+        i, j, d = self.get_matrix_indices(x.shape)
+        cols = img[:, d, i, j]
+        cols = np.concatenate(cols, axis=-1)
+        return cols
+
+    def col2im(self, dx_col, x_shape):
+        N , C, H, W = x_shape
+        H_pad = H + 2*self.padding
+        W_pad = W + 2*self.padding
+
+        X_pad = np.zeros((N, C, H_pad, W_pad))
+
+        i, j, d = self.get_matrix_indices(x_shape)
+
+        cols = np.array(np.hsplit(dx_col, N))
+
+        np.add.at(X_pad, (slice(None), d, i, j), cols)
+
+        if self.padding == 0:
+            return X_pad
+        return X_pad[:, :, self.padding:-self.padding, self.padding:-self.padding]
+
+    def forward(self, x):
+        _ , _ , H, W = x.shape
+        fast_possible = (self.kernel_size == self.stride) or (H % self.kernel_size == 0 and W % self.kernel_size == 0)
+        if fast_possible:
+            self.method = 'faster'
+            return self.forward_faster(x)
+        else:
+            self.method = 'fast'
+            return self.forward_fast(x)
+
+
+    def forward_fast(self, x):
+        N, C , H_in, W_in = x.shape
+        H_out = (H_in + 2*self.padding - self.kernel_size) // self.stride + 1
+        W_out = (W_in + 2*self.padding - self.kernel_size) // self.stride + 1
+
+        X_split = x.reshape(N * C, 1, H_in, W_in)
+        X_col = self.im2col(X_split)
+
+        max_idx = np.argmax(X_col, axis=0)
+        out = X_col[max_idx, np.arange(max_idx.size)]
+        out = out.reshape(N, C, H_out, W_out)
+        self.cache = (x, X_col, max_idx)
+        return out
+
+    def forward_faster(self, x):
+        N, C, H_in, W_in = x.shape
+        
+        assert self.kernel_size == self.stride, "kernel size must be equal to stride for fast implementation"
+        assert H_in % self.kernel_size == 0, "height must be divisible by kernel size"
+        assert W_in % self.kernel_size == 0, "width must be divisible by kernel size"
+
+        H_out = H_in // self.kernel_size
+        W_out = W_in // self.kernel_size
+
+        x_split = x.reshape(N, C, H_out, self.kernel_size, W_out, self.kernel_size)
+        out = x_split.max(axis=(3, 5))
+        self.cache = (x, x_split)
+        return out
+
+    def backward(self, d_out):
+        x, X_col = self.cache
+
+        dX_col = np.zeros_like(X_col)
+        max_idx = np.argmax(X_col, axis=0)
+        dX_col[max_idx, np.arange(max_idx.size)] = d_out.reshape(-1)
+
+        dX = self.col2im(dX_col, x.shape)
+
+        return dX 
+        
+if __name__ == '__main__':
+    # conv = Conv2D(3, 8, 3, 1, 1) # in_channels, num_filters, kernel_size, stride, padding
+    # x = np.random.randn(10, 3, 32, 32) # N, C, H, W
+    # out = conv.forward(x)
+    # print(out.shape)
+
+    # ## test backward
+    # dout = np.random.randn(10, 8, 32, 32)
+    # dx, dw, db = conv.backward(dout)
+    # print(dx.shape, dw.shape, db.shape)
+
+    # ## print numpy array to file
+    # np.set_printoptions(threshold=np.inf)
+    # np.savetxt('out_FAST_v1_fw.txt', out.reshape(-1), fmt='%.6f')
+
+    maxpool = MaxPool2D(2, 2)
+    # x = np.random.randn(10, 3, 32, 32) # N, C, H, W
+    ## generate random input integer
+    # x = np.random.randint(0, 100, size=(1, 2, 4, 4))
+    # x = np.random.randn(1, 1, 8, 8) # N, C, H, W
+    x = np.random.randn(10, 3, 32, 32) # N, C, H, W
+    # print(x)
+    out = maxpool.forward_fast(x)
+    # print(out)
+    print(out.shape)
+    
+
+    np.set_printoptions(threshold=np.inf)
+    np.savetxt('maxpool_FAST_v1_fwfast_out.txt', out.reshape(-1), fmt='%.6f')
